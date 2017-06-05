518736edd04da1dbd28a852d1b95a92ac5303fd8
Yuanhong Peng <pengyuanhong@huawei.com>
2017-06-05 20:12:45 +0800

    Always save own namespace paths
    fix #1476
    
    If containerA shares namespace, say ipc namespace, with containerB, then
    its ipc namespace path would be the same as containerB and be stored in
    `state.json`. Exec into containerA will just read the namespace paths
    stored in this file and join these namespaces. So, if containerB has
    already been stopped, `docker exec containerA` will fail.
    
    To address this issue, we should always save own namespace paths no
    matter if we share namespaces with other containers.
    
    Signed-off-by: Yuanhong Peng <pengyuanhong@huawei.com>

--- libcontainer/configs/namespaces_unix.go
+++ libcontainer/configs/namespaces_unix.go
@@ -81,9 +81,6 @@ type Namespace struct {
 }
 
 func (n *Namespace) GetPath(pid int) string {
-	if n.Path != "" {
-		return n.Path
-	}
 	return fmt.Sprintf("/proc/%d/ns/%s", pid, NsName(n.Type))
 }
 
