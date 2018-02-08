ee007187e2d1921557d8c1036e2b5a42306d8e17
Antonio Murdaca <runcom@redhat.com>
2018-02-08 13:03:45 +0100

    BACKPORT: Fix race against systemd
    Upstream reference:
    https://github.com/opencontainers/runc/pull/1683/commits/d5b4a3eddbe4c890843da971b64f45a0f023f4db
    
    Signed-off-by: Antonio Murdaca <runcom@redhat.com>

--- libcontainer/cgroups/systemd/apply_systemd.go
+++ libcontainer/cgroups/systemd/apply_systemd.go
@@ -167,9 +167,11 @@ func (m *Manager) Apply(pid int) error {
 		return err
 	}
 
-	if _, err := theConn.StartTransientUnit(unitName, "replace", properties, nil); err != nil {
+	statusChan := make(chan string)
+	if _, err := theConn.StartTransientUnit(unitName, "replace", properties, statusChan); err != nil {
 		return err
 	}
+	<-statusChan
 
 	if err := joinCgroups(c, pid); err != nil {
 		return err
